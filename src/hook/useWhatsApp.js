b514d6068678cf0506b71fc3223a8822789313f9
Jorge Altamirano <alojorge13@gmail.com>
2025-12-02 17:13:13 -0300

    agregando emogis

--- src/hook/useWhatsApp.js
+++ src/hook/useWhatsApp.js
@@ -6,28 +6,37 @@ export const useWhatsApp = () => {
     console.log("Valor de cel, metodoEntrega,metodoPago, direccion", cel, metodoEntrega, metodoPago, direccion);
     console.log("Valor de pedidos en useWhatsApp", pedido);
     const fecha = new Date().toLocaleString('es-PE', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit'
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
     })
+
+    const iconoEntrega = metodoEntrega === "local" ? "🏪" : "🛵💨";
+
+    const iconosPago = {
+      "Efectivo": "💵",
+      "Transferencia": "💳",
+    };
+
+    const iconoPago = iconosPago[metodoPago] || "💰";
 
     const mensaje = `¡NUEVO PEDIDO!\n\n` +
       `Fecha: ${fecha}\n` +
-      `Forma de entrega: ${metodoEntrega === 'local' ? 'Retira en el local' : 'Envió a domicilio'}\n` +
-      `Método de pago: ${metodoPago}\n` +
+      `Forma de entrega:  ${iconoEntrega} ${metodoEntrega === 'local' ? 'Retira en el local' : 'Envió a domicilio'}\n` +
+      `Método de pago: ${iconoPago} ${metodoPago}\n` +
       (metodoEntrega === 'envienmelo' ? `Ubicación: ${direccion}\n` : '') +
       `Pedido:\n${pedido.map(p => {
         const lineaPrincipal = ` ${p.tamanio || p.price == 0 ? "" : p.cant + 'x'} ${p.name} ${p.tamanio || p.price == 0 ? "" : '$' + p.priceVariable}`;
         const variantes = p.variantes?.length > 0
           ? `${p.variantes[0].nombre}:\n` + Object.entries(p.variantesOpcionesSelecionadas).map(([nombre, valor]) =>
-            `  ${valor.cantOpciones}x ${nombre} ${valor.valor == 0 ? "" : '$'+valor.valor} `
+            `  ${valor.cantOpciones}x ${nombre} ${valor.valor == 0 ? "" : '$' + valor.valor} `
           ).join('\n')
           : '';
         return `${lineaPrincipal}${variantes ? '\n' + variantes : ''}`;
       }).join('\n-------------------\n')}\n\n` +
-      `Total: $${pedido.reduce((sum, item) => sum + (item.totalComida ? Number(item.totalComida) : Number(item.priceVariable)),0)}`;
+      `Total: $${pedido.reduce((sum, item) => sum + (item.totalComida ? Number(item.totalComida) : Number(item.priceVariable)), 0)}`;
 
     // Codificación correcta del mensaje (conserva emojis)
     const mensajeCodificado = encodeURIComponent(mensaje)
